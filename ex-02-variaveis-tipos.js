90649ba06c401e15bed1e1a638671abe9a879bb2
lucascaio-art <lucas.caio@nave.org.br>
2026-03-05 13:49:23 -0300

    respondido ex01 e ex02

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,62 +37,63 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = "love never felt so good"
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer = { nome: "Objeto", valor: 10 }
 
 /* Questão 03 */
-export let valorIndefinido
+export let valorIndefinido = undefined
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao = false
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
-export let umaFuncao
+export let umaFuncao = function () { return "Olá mundo" }
+
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 20
 
 /* Questão 09 */
 
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "escola estadual cicero dias"
 /* Questão 10 */
 
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 20
 /* Questão 11 */
 
-export let valorFalso
+export let valorFalso = false
 /* Questão 12 */
 
-export let dezMaisQuatro
+export let dezMaisQuatro = 14
 
 /* Questão 13 */
-export let nomeDoSeuEstado
+export let nomeDoSeuEstado = "pernambuco"
 
 /* Questão 14 */
-export let artistaFavorito
+export let artistaFavorito = "michael jackson"
 
 /* Questão 15 */
-export let seteVezesSete
+export let seteVezesSete = 49
 
 /* Questão 16 */
-export let umMaiorQueDoisVerdadeOuNao
+export let umMaiorQueDoisVerdadeOuNao = false
 
 /* Questão 17 */
-export let comidaFavorita
+export let comidaFavorita = "lasanha"
 
 /* Questão 18 */
-export let oCeuEhAzulVerdadeOuNao
+export let oCeuEhAzulVerdadeOuNao = true
 
 /* Questão 19 */
-export let objetoNulo
+export let objetoNulo = null
 
 /* Questão 20 */
-export let valorVerdadeiro
+export let valorVerdadeiro = true
