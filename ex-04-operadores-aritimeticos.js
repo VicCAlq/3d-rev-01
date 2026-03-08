2d1e2d34da49530be5c69b6b9f7159226251034a
joao <joao.philippe@nave.org.br>
2026-03-08 16:26:08 -0300

    exercício 3

--- ex-04-operadores-aritimeticos.js
+++ ex-04-operadores-aritimeticos.js
@@ -85,13 +85,13 @@ export const quarentaEOito = (2 ** 3) * 6
 export const trintaETres = 66 / 2
 
 /* Questão 17 */
-export const quarentaECinco = (3 ?? 2) ?? 5
+export const quarentaECinco = (3 ** 2) * 5
 
 /* Questão 18 */
-export const oitentaENove = (9 ?? 10) ?? (5 ?? 5)
+export const oitentaENove = (9 * 10) - (5 / 5)
 
 /* Questão 19 */
-export const quarentaESeis = 23 ?? (7 ?? 5)
+export const quarentaESeis = 23 * (7 - 5)
 
 /* Questão 20 */
-export const centoEOitentaEQuatro = 23 ?? (2 ?? 4)
+export const centoEOitentaEQuatro = 23 * (2 * 4)
